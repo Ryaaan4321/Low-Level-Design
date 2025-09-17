2e3e4e6fb0321d4774e0010c35af94a071e21b6e
ryaaan4321 <aryan056400@gmail.com>
2025-09-18 00:00:12 +0530

    methods of the class

--- classs/index.java
+++ classs/index.java
@@ -9,11 +9,24 @@ class Car{
         speed=0;
         gears=0;
     }
+    public void blow_horn(){
+        System.out.println("blowing horn!");
+    }
+    public void turn_on(){
+        System.out.println("turning on the engine!");
+    }
+    public void turn_off(){
+        System.out.println("turning off the engine!");
+    }
 }
 class Main {
     public static void main(String[] args) {
         Car alto=new Car();
+        alto.turn_off();
+        alto.turn_on();
         Car mercdes=new Car();
+        mercdes.turn_off();
+        mercdes.turn_on();
         mercdes.chairs=4;
         mercdes.tyres=4;
         mercdes.speed=400;
@@ -23,6 +36,6 @@ class Main {
         alto.speed=200;
         alto.gears=4;
         
-        System.out.println(c.chairs);
+        System.out.println(alto.chairs);
     }
 }
