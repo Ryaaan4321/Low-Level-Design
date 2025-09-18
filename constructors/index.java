23f09ee1900e86c5cfe841ce55aad5607fb42f34
ryaaan4321 <aryan056400@gmail.com>
2025-09-18 23:49:01 +0530

    copy constructors

--- constructors/index.java
+++ constructors/index.java
@@ -32,14 +32,33 @@ class Movie3{
     }
 
 }
+class Movie4{
+    private String title;
+    private int duration;
+    public Movie4(String title,int duration){
+        this.title=title;
+        this.duration=duration;
+    }
+    public Movie4(Movie4 other){
+        this.title=other.title;
+        this.duration=other.duration;
+    }
+    public void displayDetails(){
+        System.out.println("movie name is " + title + " and its duration is "+ duration + " minutes");
+    }
+}
 class Main {
     public static void main(String[] args) {
-        Movie f1=new Movie();
+        // Movie f1=new Movie();
         // f1.displayDetails();
-        Movie2 f2=new Movie2();
+        // Movie2 f2=new Movie2();
         // f2.displayDetails();
-        Movie3 f3=new Movie3("fast & furious",240);
-        f3.displayDetails();
+        // Movie3 f3=new Movie3("fast & furious",240);
+        // f3.displayDetails();
+        // Movie4 f4=new Movie4();
+        Movie4 original=new Movie4("Inception",240);
+        Movie4 copy=new Movie4(original);
+        copy.displayDetails();
 
     }
 }
